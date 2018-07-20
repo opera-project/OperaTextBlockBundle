01e529f81c9ac02ae15a9b617f7d5bd43f915ff5
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-07-20 12:51:56 +0200

    Prepare admin form

--- BlockType/Text.php
+++ BlockType/Text.php
@@ -4,11 +4,19 @@ namespace Opera\TextBlockBundle\BlockType;
 
 use Opera\CoreBundle\BlockType\BaseBlock;
 use Opera\CoreBundle\BlockType\BlockTypeInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class Text extends BaseBlock implements BlockTypeInterface
 {
     public function getType() : string
     {
         return 'text';
+    }
+
+    public function createAdminConfigurationForm(FormBuilderInterface $builder)
+    {
+        $builder->add('text', TextareaType::class, [
+            'required' => true,
+        ]);
     }
 }
