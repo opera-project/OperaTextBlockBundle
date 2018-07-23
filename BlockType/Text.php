29c52c0a824d11e276a55dbb8c1ca967f52f2902
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-07-23 16:19:18 +0200

    Recomend to use ckeditor

--- BlockType/Text.php
+++ BlockType/Text.php
@@ -4,7 +4,7 @@ namespace Opera\TextBlockBundle\BlockType;
 
 use Opera\CoreBundle\BlockType\BaseBlock;
 use Opera\CoreBundle\BlockType\BlockTypeInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Opera\CoreBundle\Form\Type\CkEditorOrTextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class Text extends BaseBlock implements BlockTypeInterface
@@ -21,6 +21,6 @@ class Text extends BaseBlock implements BlockTypeInterface
 
     public function createAdminConfigurationForm(FormBuilderInterface $builder)
     {
-        $builder->add('text', TextareaType::class);
+        $builder->add('text', CkEditorOrTextareaType::class);    
     }
 }
