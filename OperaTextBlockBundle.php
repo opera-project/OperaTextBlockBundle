2c982742121b531db745dc33b756250d792be6e4
cedric lombardot <cedric.lombardot@gmail.com>
2018-08-07 16:32:19 +0200

    Update OperaTextBlockBundle.php

--- OperaTextBlockBundle.php
+++ OperaTextBlockBundle.php
@@ -4,7 +4,6 @@ namespace Opera\TextBlockBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Opera\CoreBundle\DependencyInjection\Compiler\BlockTypePass;
 
 class OperaTextBlockBundle extends Bundle
 {
