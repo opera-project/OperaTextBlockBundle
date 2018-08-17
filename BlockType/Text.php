ab01fa75f70681cc731ee0bbd59914cfacca3b45
Nsy <noelie@donkeycode.com>
2018-08-17 14:32:14 +0200

    add configure()

--- BlockType/Text.php
+++ BlockType/Text.php
@@ -7,6 +7,7 @@ use Opera\CoreBundle\BlockType\BlockTypeInterface;
 use Opera\CoreBundle\Form\Type\CkEditorOrTextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Opera\CoreBundle\Entity\Block;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 
 class Text extends BaseBlock implements BlockTypeInterface
 {
@@ -23,5 +24,13 @@ class Text extends BaseBlock implements BlockTypeInterface
     public function createAdminConfigurationForm(FormBuilderInterface $builder)
     {
         $builder->add('text', CkEditorOrTextareaType::class);    
+    }
+
+    public function configure(NodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->scalarNode('text')->defaultValue('')->end()
+            ->end();
     }
 }
